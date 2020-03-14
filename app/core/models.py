5225e9bc31ff0a88b1cfc5c8fa9e3d2ee7d2136e
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-03-14 15:35:30 +0300

    Add user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_filelds):
+        """Create and save a new user"""
+        user = self.model(
+            email=email,
+            **extra_filelds
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custome user model that supports using email instead of username"""
+    email = models.EmailField(max_length=25, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
